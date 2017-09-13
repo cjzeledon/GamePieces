a3ff9568b39129def353c00b93239b1767d084d6
Claudia Zeledon <cjzeledon@gmail.com>
2017-09-12 23:11:14 -0400

    Additional changes to the Main.java file

--- src/com/TheIronYard/GamePiece.java
+++ src/com/TheIronYard/GamePiece.java
@@ -1,14 +1,9 @@
 package com.TheIronYard;
-import java.util.*;
+import java.util.Random;
+
 
 public class GamePiece {
 
-    // In the src directory create a GamePiece Java class
-    // Add the following required instance variables
-    // int positionX
-    // int positionY
-    // boolean frozen
-    // Add a couple String instance variables (e.g. name, color)
     // Set of fields (or are these called required instance variables?)
     // This set of fields is visible to the entire class itself
     private Integer positionX;
@@ -19,24 +14,12 @@ public class GamePiece {
 
     // Add an empty constructor (no parameters). (IntelliJ can generate these for you)
     // In the constructor set the position variables to zero and frozen to false.
+    // When setting the Class and then the public, the variable is usually the same variable
     public GamePiece() {
         positionX = 0;
         positionY = 0;
-        frozen = false;
+        frozen = false; // This means that it is NOT frozen
     }
-
-    // Add get methods only for the "position" and "frozen" variables (IntelliJ can generate these for you)
-    // Add get and set methods for the other variables (IntelliJ can generate these for you)
-
-    // Add some behavior to the game piece:
-
-    // Add a move() method (returns void) that takes a new x and y position. Set the instance variables to the parameters.
-    // once you make your object it will have x = 0 and y = 0
-    // the move method will reset x = ? and y = ?
-    // Add a freeze() method (returns void) and set frozen to true
-    // Add a unfreeze() method (returns void) and set frozen to false
-    // Change move() so that that when frozen the piece does not move
-    // Test the freeze, unfreeze and move methods in the public static void main(String[] args) method of Main.java.
 
     public Integer getPositionX() {
         return positionX;
@@ -66,43 +49,58 @@ public class GamePiece {
         this.color = color;
     }
 
+    // When the move method has a parameter set (i.e. "Integer X, Integer Y"), it means that you are telling the computer that you will
+    // provide the inputs to that parameter.
     public void move(Integer X, Integer Y) {
         // look at the frozen field
         // if the frozen field = true move the piece
         // if the frozen field = false don't allow the piece to move
-        if (this.frozen == false) {
+        if (this.frozen == true) {
             this.positionX = X;
             this.positionY = Y;
+            System.out.println("The manual input for X position is: " + this.positionX);
+            System.out.println("The manual input for Y position is: " + this.positionY);
         } else {
-            System.out.println("You are frozen");
+            System.out.println("You are so manually frozen!");
         }
     }
 
+    // The parameter of move method does not need to be assigned if you simply want the computer to move pass and run
+    // the next block of code within that method.
+    // You can easily set the X.nextInt() as it is and it will give you LARGE random numbers (and weird)
+    // Otherwise, set X.nextInt(100) which will give you the random number up to 99. If you want 100, you need to add a plus 1 which will
+    // look like this X.nextInt(100)+1
+    // Random X = new Random();
+    // Random is a type of object
+    // X is variable name
+    // Random() is new constructor
+    public void moveRandom() {
+        if (this.frozen == true) {
+            Random X = new Random();
+            this.positionX = X.nextInt();
+            Random Y = new Random();
+            this.positionY = Y.nextInt();
+        } else {
+            System.out.println("You are randomly frozen!");
+        }
+        System.out.println("The randomized X position is: " + this.positionX);
+        System.out.println("The randomized Y position is: " + this.positionY);
+    }
+
     // Creates a method that initialize instance variables
-    // This returns void
+    // Since the method contains a void, it returns nothing
+    // Add a freeze() method (returns void) and set frozen to true
     public void freeze() {
-        this.frozen = true;
+        this.frozen = true; // Indicates that freeze IS frozen
     }
 
     // Creates a method that initialize instance variables
-    // This returns void
+    // Since this method contains void, it will not return anything
     public void unfreeze() {
-        this.frozen = false;
+        this.frozen = false; // Indicates that the unfreeze is NOT frozen
     }
 
-
-    // Create a GamePiece object.
-
-    // Only call the methods on the object, do not access the instance variables directly.
-    // Verify that if the game piece is not frozen then the call to freeze sets the frozen varable to true.
-    // Verify that if the game piece is frozen then the call to unfreeze sets the frozen varable to false.
-    // Verify that if the game piece is not frozen then the call to move results in the new position.
-    // Verify that if the game piece is frozen then the call to move does not change the position.
-
-    // Extras
-    // Use the Random.nextInt method to randomly get values for x and y when calling move.
-
-    // More Extras
+    // Extras - Incomplete
     // Add four instance variables (minX, maxX, minY, maxY) to define the bounds of a grid confining the game piece (e.g. x is bounded by zero and 100 and y is bounded by zero and 500). Set the values in the constructor. Add getter methods.
     // Refactor the move() method to not allow the game piece to move outside the grid (move the piece to the border of the grid but not beyond).
     // Test the movement in the main method of Main.java.
--- src/com/TheIronYard/Main.java
+++ src/com/TheIronYard/Main.java
@@ -3,7 +3,8 @@ package com.TheIronYard;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // Create a GamePiece object
+        // Only call the methods on the object, do not access the instance variables directly.
         GamePiece hitIt = new GamePiece();
 
 //        hitIt.move(10, 50);
@@ -16,13 +17,38 @@ public class Main {
 //        hitIt.unfreeze();
 //        hitIt.move(4, 2);
 
-        hitIt.freeze();
-
-        hitIt.move(3,5);
-
+        System.out.println("Verify that if the game piece is not frozen then the call to freeze sets the frozen variable to true.");
         hitIt.unfreeze();
+        hitIt.moveRandom();
+        hitIt.move(9,4);
+        hitIt.freeze();
+        System.out.println("The frozen variable is now set to " + hitIt.frozen);
 
-        hitIt.move(3,5);
+        System.out.println("Verify that if the game piece is frozen then the call to unfreeze sets the frozen variable to false.");
+        hitIt.freeze();
+        hitIt.moveRandom();
+        hitIt.move(3,10);
+        hitIt.unfreeze();
+        System.out.println("The frozen variable is now set to " + hitIt.frozen);
+
+        System.out.println("Verify that if the game piece is frozen then the call to unfreeze sets the frozen variable to false.");
+        hitIt.freeze();
+        hitIt.moveRandom();
+        hitIt.move(24,45);
+        hitIt.unfreeze();
+        System.out.println("The frozen variable is now set to " + hitIt.frozen);
+
+        System.out.println("Verify that if the game piece is not frozen then the call to move results in the new position.");
+        hitIt.unfreeze();
+//        hitIt.move(32,87);
+//        hitIt.freeze();
+//        System.out.println("The frozen variable is now set to " + hitIt.frozen);
+
+        System.out.println("Verify that if the game piece is frozen then the call to move does not change the position.");
+//        hitIt.unfreeze();
+//        hitIt.move(32,87);
+//        hitIt.freeze();
+//        System.out.println("The frozen variable is now set to " + hitIt.frozen);
 
     }
 }
